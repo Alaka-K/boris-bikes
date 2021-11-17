4547a8031da5970b1f7d29f5107a4b9ef3325c7d
kehinde alaka <kennyalaka93@gmail.com>
2021-11-17 16:03:04 +0000

    correct previous step as code needed to be in docking station

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,5 +3,8 @@ require "bike"
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
-    it { expect(subject.release_bike).to be_instance_of Bike }
+    it 'releases working bikes' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+    end
 end
