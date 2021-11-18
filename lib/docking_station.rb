c86fce07f45ea38c4cd08ff1739cac2d81bf5328
Jessica Gordon <ketchesj@gmail.com>
2021-11-18 17:08:34 +0000

    Step 12 & 13: Added exceptions for release bike and dock bike with capacities of 1

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,10 +2,11 @@ require_relative 'bike'
 class DockingStation
   attr_reader :bike
   def release_bike
-      bike = Bike.new
+    fail "Docking station empty!" unless @bike
+    @bike
   end
   def dock(bike)
+  fail "Docking station full" if @bike
   @bike = bike
   end
-
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,15 +4,31 @@ require "bike"
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
     it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+        bike = Bike.new
+        subject.dock(bike)
+        bike = subject.release_bike
+        expect(bike).to be_working
     end
+
     it { is_expected.to respond_to(:dock).with(1).argument }
     it { is_expected.to respond_to(:bike) }
 
-    it "returns docked bike" do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+    describe '#release_bike' do
+
+        it "returns docked bike" do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike).to eq bike
+        end
+
+        it "raise error when docking_station.release_bike is empty" do
+            expect { subject.release_bike }.to raise_error "Docking station empty!"
+        end
+        it "raise error when docking_station is full" do
+            bike = Bike.new
+            subject.dock(bike)
+            bike2 = Bike.new
+            expect {subject.dock(bike2)}.to raise_error "Docking station full"
+        end
     end
 end
