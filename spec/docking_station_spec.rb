cfacfc92280c30f5d1b90f6c7fe735cb00f8fd1d
Christopher <dagswebb@gmail.com>
2021-11-18 14:52:33 +0000

    Added Exception

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,16 +3,32 @@ require "bike"
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike }
-    it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+
+    it 'expect bikes to be working' do
+    bike = Bike.new
+    expect(bike.working?).to eq true
     end
+
     it { is_expected.to respond_to(:dock).with(1).argument }
     it { is_expected.to respond_to(:bike) }
 
-    it "returns docked bike" do
+    it 'returns docked bike' do
     bike = Bike.new
     subject.dock(bike)
     expect(subject.bike).to eq bike
+    end
+
+    describe '#release_bike' do
+        it 'releases a bike' do
+            bike = Bike.new
+            subject.dock(bike)
+            expect(subject.release_bike).to eq bike
+        end
+    end
+
+    describe '#release_bike' do
+        it 'should not release a bike when none are available' do
+            expect { subject.release_bike }.to raise_error 'No bikes available'
+        end
     end
 end
