48b182bdad4dde8385e24c2a4de24f160b0748e1
Jessica Gordon <ketchesj@gmail.com>
2021-11-19 17:05:20 +0000

    Step 15: Refactored code using SRP and private methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -7,12 +7,22 @@ class DockingStation
   end
 
   def release_bike
-    fail "Docking station empty!" if @bikes.empty?
+    fail "Docking station empty!" if empty?
     @bikes.pop
   end
   
   def dock(bike)
-    fail "Docking station full" if @bikes.length == 20
+    fail "Docking station full" if full?
     @bikes.push(bike)
+  end
+
+  private
+
+  def full?
+    @bikes.length >= 20 ? true : false
+  end
+
+  def empty?
+    @bikes.length == 0 ? true : false
   end
 end
